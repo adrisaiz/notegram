2bd0a5a58d20f046a7e8feff15a405d692d7a482
adrian.saiz <adrian.saiz@britebill.com>
2018-11-18 13:38:28 -0800

    Added passport-jwt usage

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb://adri:FTIa4YIRfnfW@ds251223.mlab.com:51223/adrisaiz'
+    mongoURI : 'mongodb://adri:FTIa4YIRfnfW@ds251223.mlab.com:51223/adrisaiz',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const notes = require('./routes/api/notes');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
@@ -12,7 +17,10 @@ mongoose.connect(db)
 .then(()=> console.log('MongoDB connected'))
 .catch(error => console.log(error))
 
-app.get('/', (req, res) => res.send('hello!!'));
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
 
 app.use('/api/users', users);
 app.use('/api/notes', notes);
