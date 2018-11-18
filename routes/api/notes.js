80c65b122dfc94914f34263f4a8cd3862c562184
adrian.saiz <adrian.saiz@britebill.com>
2018-11-18 13:40:47 -0800

    Updated API routes
    Added register new user, login existing user and get current list of users.
    Added get all notes, one note, post one note, remove one note.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport-jwt');
+
+const Note = require('../../models/Note');
+const validateNoteInput = require('../../validation/note')
 
 router.get('/test', (req,res) => res.json({msg: "Notes works"}));
+
+
+router.get('/:Id', passport.authenticate('jwt',{session: false}), (req, res)=>{
+    Note.findById(req.param.id)
+    .then(notes => res.json(notes))
+    .catch(res.status(404).json({nonotefound: 'No note found with that ID'}));
+});
+
+router.get('/', passport.authenticate('jwt',{session: false}), (req, res)=>{
+    Note.find()
+    .sort({date: -1})
+    .then(notes => res.json(notes))
+    .catch(res.status(404).json({nonotesfound:'No notes found'}));
+});
+
+router.post('/', passport.authenticate('jwt',{session: false}), (req, res)=>{
+    const {errors, isValid} = validateNoteInput(req.body);
+
+    if (!isValid){
+        return res.status(400).json(errors);
+    }
+    const newNote = new Note({
+        text: req.body.text,
+        name: req.body.name,
+        user: req.user.id
+    });
+    newNote.save().then(note => res.json(note))
+});
+
+router.delete('/:Id', passport.authenticate('jwt', {session: false}),(req,res)=>{
+    Note.findById(req.param.id)
+    .then(note => {
+        if (note.user.toString() !== req.user.id){
+            return res.status(401).json({notauthorised: 'User not authorised'});
+        } 
+        note.remove().then(()=> res.json({success: true}))
+        .catch(err => res.status(404).json({notenotfound: 'Note not found'}));
+    })
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,82 @@
 const express = require('express');
 const router = express.Router();
+const bCrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const config = require('../../config/keys');
+
+const User = require('../../models/User');
+
+const ValidateRegisterInput = require('../../validation/register');
+const ValidateLoginInput = require('../../validation/login')
 
 router.get('/test', (req,res)=>res.json({msg: "Users works"}));
+
+router.post('/register', (req,res) => {
+    const {errors, isValid} = ValidateRegisterInput(req.body);
+    
+    if (!isValid) {
+        return res.status(400,errors);
+    }
+    User.findOne({email: req.body.email })
+    .then(user => {
+        if (user){
+            errors.email = 'Email already exists';
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({name: req.body.name, email: req.body.email, password: req.body.password});
+            bCrypt.genSalt(10, (error, salt) => {
+                bCrypt.hash(newUser.password, salt, (error, hash) => {
+                    if (error) throw error;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(error => console.log(error));
+                });
+            });
+        }
+
+    })
+    .catch((error)=> console.log(error));
+});
+
+router.post('/login', (req, res) => { 
+    const email = req.body.email;
+    const password = req.body.password;
+    const {errors, isValid} = ValidateLoginInput(req.body);
+    User.findOne({email: email}).
+    then((user)=> { 
+        if (!user) {
+            errors.email = 'User email not found';
+            return res.status(404).json(errors);
+        }
+        bCrypt.compare(password, user.password)
+        .then((isMatch)=>{
+            if (isMatch){
+                const payload = {id: user.id, name: user.name};
+                jwt.sign(
+                    payload,
+                    config.secretOrKey, 
+                    {expiresIn: 3600}, (err, token)=>{
+                        res.json({
+                            success: true,
+                            token: 'Bearer '+ token
+                        })
+                });
+            } else {
+                errors.password = 'Password incorrect';
+                return res.status(400).json(errors);
+            }
+        })
+    })
+})
+
+router.get('/current', passport.authenticate('jwt', {session: false}) ,(req, res)=>{
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+})
 
 module.exports = router;
